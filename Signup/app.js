92bc137308432e41e796988138d9cc11a65d0f3e
Mike_Kowulich <Mike_Kowulich@bitbucket.org>
2020-11-06 13:42:14 -0500

    Front End Validation

--- Signup/app.js
+++ Signup/app.js
@@ -13,3 +13,14 @@ function login() {
     y.style.left='450px';
     z.style.left='0px';
 }
+
+function validateReg() {
+    let psw = document.getElementById("psw");
+    let pswconfirm = document.getElementById("pswconfirm");
+    if (psw.value != pswconfirm.value) {
+        alert("Error: Passwords do not match.");
+        return false;
+    } else {
+        return true;
+    }
+}
--- Signup/signup.php
+++ Signup/signup.php
@@ -24,13 +24,15 @@
                 <label for="checkbox">Remember Me</label>
                 <button type="submit" class="submit-btn">Login</button>
             </form>
-            <form action="http://www.itss.brockport.edu/~mkowu1/cis442/CIS442PROJECT/Signup/register.php" method="POST" id="register" class="input-group2">
-                <input type="text" class="input-field" name="name" maxlength="50" placeholder="Full Name" required>
-                <input type="text" class="input-field" name="address" maxlength="100" placeholder="Full Address" required>
-                <input type="text" class="input-field" name="uname" maxlength="30" placeholder="User ID" required>
+            <form action="http://www.itss.brockport.edu/~mkowu1/cis442/CIS442PROJECT/Signup/register.php" onsubmit="return validateReg()" method="POST" id="register" class="input-group2">
+                <input type="text" class="input-field" name="name" maxlength="50" minlength="1" pattern="[a-z A-Z]{7-50}" placeholder="Full Name" required>
+                <input type="text" class="input-field" name="address" maxlength="100" pattern="[a-z A-Z0-9,-]{7-100}" placeholder="Full Address" required>
+                <input type="text" class="input-field" name="uname" maxlength="30" pattern="[a-zA-Z0-9]{7-50}" minlength="1" placeholder="User ID" required>
                 <input type="email" class="input-field" name="email"  maxlength="50" placeholder="Email ID" required>
-                <input type="password" class="input-field" name="psw" placeholder="Enter Password" maxlength="50" required>
-                <input type="password" class="input-field" name="pswconfirm" maxlength="50" placeholder="Confirm Password" required>
+                <input type="password" class="input-field" id="psw" name="psw" placeholder="Enter Password" 
+                title="Password must be at least 7 characters" minlength="7" maxlength="50" required>
+                <input type="password" class="input-field" id="pswconfirm" name="pswconfirm" pattern="[a-zA-Z0-9]{7-50}"minlength="7" maxlength="50" 
+                title="Password must be at least 7 characters" placeholder="Confirm Password" pattern="[a-zA-Z0-9]{7-50}" required>
                 <input type="checkbox" name="checkbox" class="check-box">
                 <label for="checkbox">Remember Me</label>
                 <button type="submit" class="submit-btn">Register</button>

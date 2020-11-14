5fd1107a40caf87ffe1872807ff4d03286943e21
nstaub20 <62774902+nstaub20@users.noreply.github.com>
2020-11-14 05:03:31 -0500

    Update account_overview.php

--- Account/account_overview.php
+++ Account/account_overview.php
@@ -8,20 +8,32 @@
     </head>
     <body>
  <form>
+  
   <label for="Name">Name</label><br>
   <input type="text" id="Name" name="Name" size="15" maxlength="30"><br>
+  
   <label for="address">Street Address</label><br>
   <input type="text" id="address" name="address" size="15" maxlength="30"><br>
+  
   <label for="eaddress">Email Address</label><br>
   <input type="text" id="eaddress" name="eaddress" size="15" maxlength="30"><br>
+  
   <label for="pnumber">Phone Number</label><br>
   <input type="text" id="pnumber" name="pnumber" size="15" maxlength="30"><br>
+  
   <label for="city">City</label><br>
   <input type="text" id="city" name="city" size="15" maxlength="30"><br>
+  
   <label for="state">State</label><br>
   <input type="text" id="state" name="state" size="15" maxlength="30"><br>
+  
   <label for="zip">Zip Code</label><br>
   <input type="text" id="zip" name="zip" size="15" maxlength="30"><br>
+  
+  <label for="baddress">Billing Address</label><br>
+  <input type="text" id="baddress" name="baddress" size="15" maxlength="30"><br>
+     
+   
         </form>
   
     </body>

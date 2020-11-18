02b7ff58e8ae4693bb53a38ca14c2c6eedbe86a5
Mike_Kowulich <Mike_Kowulich@bitbucket.org>
2020-11-18 13:15:31 -0500

    Dashboard Changes

--- Dashboard/dashboard.php
+++ Dashboard/dashboard.php
@@ -9,9 +9,13 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
     <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="dashboard.css">
     <script>
         function homescreen() {
             window.location.href = "../index.php";
+        }
+        function shop() {
+            window.location.href = "../Shop/shop.php"
         }
     </script>
     <title>Hack N` Snacks Dashboard</title>
@@ -33,8 +37,47 @@
              <li class='navlink'><a href='../Shop/shop.php'>Shop</a></li>
              <li class='navlink'><a href='../Signup/logout.php'>Logout</a></li>
              </ul>
-            </header>";
+            </header><br><br><br><br>
+        <div class='centralize'>
+            <h1 class='jumbo'>My Subscriptions</h1>";
         }
-    ?>
+        ?>
+        <?php
+            if (!isset($_SESSION['username'])) {
+                exit;
+            }
+            require_once('../connection.php');
+            $uname = $_SESSION['username'];
+            $uid = $_SESSION['id'];
+            $sql = "SELECT * FROM subscription WHERE userId='$uid' AND status='Active'";
+            $result = mysqli_query($conn, $sql);
+            if(mysqli_num_rows($result) > 0) {
+                while($row = mysqli_fetch_assoc($result)) {
+                    $type = $row["type"];
+                    $cost = $row["cost"];
+                    echo "<div class='subBox'>
+                            <img src='../img/box.png' alt='HackNSnacks Box'><br><br><br>
+                            <p>Type Of Subscription: $type</p>
+                            <p>Cost: $cost </p>
+                            <br>
+                            <br>
+                            <button type='button' class='cancel-btn'>Cancel Subscription </button>
+                         </div>";
+                }
+            } else {
+                echo "<div class='subBox'>
+                            <img src='../img/box.png' alt='HackNSnacks Box'><br><br><br>
+                            <p>You don't have any active subscriptions!</p>
+                            <p>Click the button below to browse subscription options.</p>
+                            <br>
+                            <br>
+                            <button type='button' class='cancel-btn' onclick='shop()'> Shop </button>
+                         </div>";
+            }
+        ?>
+        <?php
+        echo "</div>";
+        ?>
+    
 </body>
 </html>

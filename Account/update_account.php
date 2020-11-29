8a9dbf774022746a84024df90136edf4590cfb30
nstaub20 <62774902+nstaub20@users.noreply.github.com>
2020-11-29 01:33:41 -0500

    Update update_account.php

--- Account/update_account.php
+++ Account/update_account.php
@@ -1,1 +1,14 @@
-
+<?php 
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    exit;
+}
+$Name = $_POST['Name'];
+$address = $_POST['address'];
+$eaddress = $_POST['eaddress'];
+$pnumber = $_POST['pnumber'];
+$city = $_POST['city'];
+$state = $_POST['state'];
+$zip = $_POST['zip'];
+$baddress = $_POST['baddress'];
+$cnumber = $_POST['cnumber'];
+$CCV = $_POST['CCV'];

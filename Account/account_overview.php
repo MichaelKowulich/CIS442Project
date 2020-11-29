42f0d54a8a222e254aa99446649c938400895de5
nstaub20 <62774902+nstaub20@users.noreply.github.com>
2020-11-29 01:21:06 -0500

    Update account_overview.php

--- Account/account_overview.php
+++ Account/account_overview.php
@@ -8,7 +8,7 @@
         <h1> Account Settings </h1>
     </head>
     <body>
- <form>
+ <form action="http://www.itss.brockport.edu/~mkowu1/cis442/CIS442PROJECT/account/account_overview.php" onsubmit="return validateReg()" method="POST" id="accountoverview" class="input-group2">
   
   <label for="Name">Name</label><br>
   <input type="text" id="Name" name="Name" size="15" maxlength="30"><br>

788ab207aa42b098da952029c34c55559beb4f13
amarino102 <44756664+amarino102@users.noreply.github.com>
2020-12-01 14:42:12 -0500

    updating about
    updating about

---
+++ About/about.php
@@ -0,0 +1,16 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="about.css">
+    <title>Hack N` Snacks About Page</title>
+</head>
+
+</html>

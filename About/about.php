b63d889eda62292b0c29671d44c9ea941a699d43
amarino102 <44756664+amarino102@users.noreply.github.com>
2020-12-01 19:41:08 -0500

    update about.php

--- About/about.php
+++ About/about.php
@@ -10,9 +10,28 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
     <link rel="stylesheet" href="../style.css">
     <link rel="stylesheet" href="about.css">
-    <title>Hack n' Snacks About Page</title>
+    <script>
+        function homescreen() {
+            window.location.href = "../index.php";
+        }
+    </script>
+    <title>Hack N' Snacks About Page</title>
 </head>
 <body>
+<header>
+<?php
+            if(!isset($_SESSION["username"])) {
+                echo '<li class="navlink"><a href="index.php">Home</a></li>';
+                echo '<li class="navlink"><a href="Signup/signup.php">Login / Sign Up</a></li>';
+            } else {
+                $username = $_SESSION['username'];
+                echo "<li class='navlink'><a href='Dashboard/dashboard.php'>Dashboard</a></li>";
+                echo "<li class='navlink'><a href='Shop/shop.php'>Shop</a></li>";
+                echo "<li class='navlink'><a href='Account/account_overview.php'>$username's Account</a></li>";
+                echo "<li class='navlink'><a href='Signup/logout.php'>Logout</a></li>";
+            }
+        ?>
+</header><br><br><br><br>
 <div class="centralize">
     <h1 class="jumbo">About Hack n' Snacks </h1> 
     <p class="jumbotron">Hack n’ Snacks provides various meals of all kinds to consumers by the click of a few buttons! 
@@ -22,11 +41,12 @@
         house. We also were aware of parents and families having to feed their kids who were home from school. The meals we 
         wanted to sell were trying to be geared towards that very situation; to make it easier, quicker, and more affordable. 
         This was so, a parent working from home would not have to worry about not having lunch or dinner every single night. 
+        <br><br>
     <h1 class="jumbo">How It Works</h1>
     <p class ="jumbotron"> The goal is for you to find the subscription that best suits you. Create a great, customizable 
         account that will keep your subscription. Then, check out the options: Standard (No food allergies or restrictions), 
         Vegan, Vegetarian, Gluten-Free, Meat-Eater and Pescatarian. Select a subscription that fits how you eat or maybe 
-            something you want to try. 
+        something you want to try. 
 </div>
 </body>
 </html>

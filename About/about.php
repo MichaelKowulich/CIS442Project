12e278c5905aaf203593ca4cac5e08eb0263f4be
amarino102 <44756664+amarino102@users.noreply.github.com>
2020-12-01 19:23:47 -0500

    update

--- About/about.php
+++ About/about.php
@@ -13,8 +13,18 @@
     <title>Hack N` Snacks About Page</title>
 </head>
 <body>
-    <h1> About Hack n' Snacks </h1> 
-
-    <h1> How It Works</h1>
+    <h1 class="jumbo">About Hack n' Snacks </h1> 
+    <p class="jumbotron">Hack n’ Snacks provides various meals of all kinds to consumers by the click of a few buttons! 
+        They are easy to make, nutritious, and most definitely delicious. Hack n’ Snacks is looking to help the parents, 
+        families, or anyone in general, who is struggling with the current pandemic we all are living through. We wanted to 
+        create this easy, affordable, meal service that you can order online so that you can avoid the hassle of leaving the 
+        house. We also were aware of parents and families having to feed their kids who were home from school. The meals we 
+        wanted to sell were trying to be geared towards that very situation; to make it easier, quicker, and more affordable. 
+        This was so, a parent working from home would not have to worry about not having lunch or dinner every single night. 
+    <h1 class="jumbo">How It Works</h1>
+    <p class ="jumbotron"> The goal is for you to find the subscription that best suits you. Create a great, customizable 
+        account that will keep your subscription. Then, check out the options: Standard (No food allergies or restrictions), 
+        Vegan, Vegetarian, Gluten-Free, Meat-Eater and Pescatarian. Select a subscription that fits how you eat or maybe 
+        something you want to try. 
 </body>
 </html>
